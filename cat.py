350d9062d905eb552cbd64e57a74838b0cf34db2
ramandhull59 <ramandhull59@gmail.com>
2024-04-19 11:39:43 +1200

    srh

---
+++ cat.py
@@ -0,0 +1,10 @@
+# Define cat attributes
+cat_name = "Whiskers"
+cat_height = 12  # in inches
+cat_color = "orange"
+
+# Print cat information
+print("Name:", cat_name)
+print("Height:", cat_height, "inches")
+print("Color:", cat_color)
+#if you want a code for input you can simply see my cat2.py
---
+++ cat2.py
@@ -0,0 +1,9 @@
+# Ask for cat attributes
+cat_name = input("Enter the cat's name: ")
+cat_height = input("Enter the cat's height (in inches): ")
+cat_color = input("Enter the cat's color: ")
+
+# Print cat information
+print("Name:", cat_name)
+print("Height:", cat_height, "inches")
+print("Color:", cat_color)
